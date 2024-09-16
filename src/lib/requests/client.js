b88bfaa88976e4773a9caf24c4a6890b56115a62
Josemaria Ofurum <Josemariaofurum@gmail.com>
2024-09-16 06:32:32 +0100

    implement error handling for tokens

--- src/lib/requests/client.js
+++ src/lib/requests/client.js
@@ -1,17 +1,52 @@
 import axios from "axios";
-import { getItemFromLs } from "../ls";
+import { addItemToLs, deletItemFromLs, getItemFromLs } from "../ls";
 
 const client = axios.create({
   baseURL: "/api",
 });
 
 export const clientRequest = async ({ ...options }) => {
-  console.log(options)
+  console.log(options);
   const accessToken = getItemFromLs("accessToken");
   client.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
   const onSuccess = (res) => res;
   const onError = (err) => {
     console.log(err);
+
+    const originalRequest = err.config;
+    const status = err.response.status;
+
+    if (status === 401 && !originalRequest._retry) {
+      console.log("access token expired", err.status);
+      originalRequest._retry = true;
+
+      try {
+        const refreshToken = getItemFromLs("refreshToken");
+
+        const {
+          data: { access: accessToken, refresh: newRefreshToken },
+        } = axios.post("/api/auth/token/refresh");
+        const isTheSame = refreshToken === newRefreshToken;
+        
+        console.log(isTheSame, "is the old rt and new rt same?");
+
+        // update tokens in ls
+        addItemToLs("accessToken", accessToken);
+        addItemToLs("refreshToken", refreshToken);
+
+        // update the request
+        originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+        return client(options)
+          .then(onSuccess)
+          .catch((err) => console.log(err));
+      } catch (error) {
+        console.log(error);
+        deletItemFromLs("accessToken");
+        deletItemFromLs("refreshToken");
+        deletItemFromLs("user");
+        return Promise.reject(err);
+      }
+    }
     // what action do we want to take
     return err;
   };
